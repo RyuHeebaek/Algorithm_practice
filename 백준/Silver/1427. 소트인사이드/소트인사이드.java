5ee4d3e237d34b818a44db005104c7e47d26c924
RyuHeebaek <88934604+RyuHeebaek@users.noreply.github.com>
2024-12-10 16:18:22 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,17 +1,16 @@
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.IOException;
-import java.io.BufferedReader;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        InputStream in = System.in;
+        
         int[] cnt = new int[10];
-        String str = br.readLine();
-
-        for(int i=0;i<str.length();i++){
-            cnt[str.charAt(i)-'0']++;
+        int c;
+        while((c=in.read()) != '\n'){
+            cnt[c-'0']++;
         }
-
+        
         for(int i=9;i>=0;i--){
             while(cnt[i]-- > 0){
                 System.out.print(i);
